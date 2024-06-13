4f0d380045d5cb07a1e3738c3defcfa2d8885993
Jevillalbav <jevillalbav@unal.edu.co>
2024-06-13 10:24:51 -0500

    last commit

--- dashboard_stream.py
+++ dashboard_stream.py
@@ -10,17 +10,33 @@ st.set_page_config(page_title="Financial Dashboard", layout="wide")
 st.title("Financial Dashboard")
 
 # Leer datos desde archivos locales
-prop_file = 'prop.csv'
-tt_file = 'trades.csv'
-metrics_file = 'metrics.csv'
+prop_file = 'https://raw.githubusercontent.com/Jevillalbav/strategy/d22d14cc755e52001a122a03a7d31585260a1a54/prop.csv' # 'prop.csv'
+tt_file = 'https://raw.githubusercontent.com/Jevillalbav/strategy/d22d14cc755e52001a122a03a7d31585260a1a54/trades.csv' # 'trades.csv'
+metrics_file ='https://raw.githubusercontent.com/Jevillalbav/strategy/d22d14cc755e52001a122a03a7d31585260a1a54/metrics.csv' # 'metrics.csv'
+
+# # Leer datos desde GitHub
+# prop_ = pd.read_csv('https://raw.githubusercontent.com/Jevillalbav/strategy/d22d14cc755e52001a122a03a7d31585260a1a54/prop.csv', index_col=0, parse_dates=True)
+# tt = pd.read_csv('https://raw.githubusercontent.com/Jevillalbav/strategy/d22d14cc755e52001a122a03a7d31585260a1a54/trades.csv', index_col=0)
+# metrics = pd.read_csv('https://raw.githubusercontent.com/Jevillalbav/strategy/d22d14cc755e52001a122a03a7d31585260a1a54/metrics.csv', index_col=0)
+
+
 
 # Leer los datos
 try:
-    prop_ = pd.read_csv(prop_file, index_col=0, parse_dates=True)
-    tt = pd.read_csv(tt_file, index_col=0)
-    metrics = pd.read_csv(metrics_file, index_col=0)
+    # prop_ = pd.read_csv(prop_file, index_col=0, parse_dates=True)
+    # tt = pd.read_csv(tt_file, index_col=0)
+    # metrics = pd.read_csv(metrics_file, index_col=0)
+
+
+    prop_ = pd.read_csv('https://raw.githubusercontent.com/Jevillalbav/strategy/d22d14cc755e52001a122a03a7d31585260a1a54/prop.csv', index_col=0, parse_dates=True)
+    tt = pd.read_csv('https://raw.githubusercontent.com/Jevillalbav/strategy/d22d14cc755e52001a122a03a7d31585260a1a54/trades.csv', index_col=0)
+    metrics = pd.read_csv('https://raw.githubusercontent.com/Jevillalbav/strategy/d22d14cc755e52001a122a03a7d31585260a1a54/metrics.csv', index_col=0)
+
+
 except Exception as e:
     st.error(f"Error loading data: {e}")
+
+
 
 # Crear gráfica de Plotly
 fig = go.Figure()
@@ -48,7 +64,7 @@ fig.update_layout(
         titlefont=dict(color='rgba(227, 202, 134,1)'),
         tickfont=dict(color='rgba(227, 202, 134,1)'),
         tickformat='$,.0f',
-        range=[1000, prop_['gold'].max() * 1.2],
+        range=[500, prop_['gold'].max() * 1.2],
         dtick=100,
         separatethousands=True
     ),
@@ -56,25 +72,30 @@ fig.update_layout(
         title='Position Sizing - Ounces',
         overlaying='y1',
         side='right',
-        position=0.8,
+        position=0.95,
         zeroline=False,
-        range=[0, 15000],
+        range=[0, 20000],
         dtick=1000,
         tickformat=',.1f',
-        visible=True
+        visible=True,
+        showgrid=False,
+        titlefont=dict(color='lightgreen'),
+        tickfont=dict(color='lightgreen')
     ),
     yaxis3=dict(
         title='Cumulated Position',
         overlaying='y1',
         side='right',
-        position=0.9,
+        position=0.85,
         hoverformat=',.0%',
         range=[-0.4, 1],
         dtick=0.1,
         showgrid=False,
         tickangle=-45,
-        zeroline=True,
-        tickformat=',.0%'
+        zeroline=False,
+        tickformat=',.0%',
+        titlefont=dict(color='lightblue'),
+        tickfont=dict(color='lightblue')
     ),
     hovermode='x',
     legend=dict(x=0.95, y=1, bgcolor='black', traceorder='grouped'),
@@ -84,15 +105,17 @@ fig.update_layout(
     showlegend=False
 )
 
-fig.update_traces(marker_line_color='rgba(0,0,0,0)')
+#fig.update_traces(marker_line_color='rgba(0,0,0,0)')
+
 fig.add_trace(go.Scatter(
     x=prop_.index,
     y=prop_['gold'],
     mode='lines+markers',
     line=dict(color='gold', width=1),
-    name='gold',
+    name='Gold',
     marker=dict(color='gold', size=4),
-    hovertemplate='%{x|%Y-%m-%d}<br>Price: %{y:$,.2f}<br>Asset: gold'
+    text= 'Gold Price',
+    hoverinfo='y+name'
 ))
 
 fig.add_trace(go.Scatter(
@@ -100,9 +123,8 @@ fig.add_trace(go.Scatter(
     y=prop_['gold'] * .99,
     mode='markers',
     marker=dict(color='lightgreen', size=prop_['long_signal_size'] * 10, symbol='triangle-up'),
-    name='long',
+    name='Long ↗',
     text=prop_['long_signal'] * 500,
-    hovertemplate='%{x|%Y-%m-%d}<br>Signal: long<br>Value: %{text}',
     hoverinfo='text+name'
 ))
 
@@ -111,9 +133,8 @@ fig.add_trace(go.Scatter(
     y=prop_['gold'] * 1.01,
     mode='markers',
     marker=dict(color='red', size=prop_['short_signal_size'] * 10, symbol='triangle-down'),
-    name='short',
+    name='Short ↘',
     text=prop_['short_signal'] * 500,
-    hovertemplate='%{x|%Y-%m-%d}<br>Signal: short<br>Value: %{text}',
     hoverinfo='text+name',
     opacity=1
 ))
@@ -122,20 +143,20 @@ fig.add_trace(go.Scatter(
     x=prop_.index,
     y=prop_['str_cum_return'],
     mode='lines',
-    line=dict(color='lightblue', width=1),
-    name='strategy',
+    line=dict(color='lightblue', width=1, dash='dot'), 
+    name='Strategy',
     yaxis='y3',
-    hovertemplate='%{x|%Y-%m-%d}<br>Cumulated Return: %{y}'
+    hoverinfo='y+name'
 ))
 
 fig.add_trace(go.Scatter(
     x=prop_.index,
     y=prop_['gold_cum_return'],
     mode='lines',
-    line=dict(color='gold', width=1),
-    name='gold',
+    line=dict(color='gold', width=1, dash='dot'),
+    name='Gold Ret%',
     yaxis='y3',
-    hovertemplate='%{x|%Y-%m-%d}<br>Cumulated Return: %{y}'
+    hoverinfo='y+name'
 ))
 
 fig.add_trace(go.Scatter(
@@ -143,12 +164,23 @@ fig.add_trace(go.Scatter(
     y=prop_['oz_exp'],
     mode='lines',
     line=dict(color='lightgreen', width=0.5),
-    name='position sizing',
+    name='Open Size',
     yaxis='y2',
     fill='tozeroy',
     fillcolor='rgba(0, 255, 0, 0.3)',
-    hovertemplate='%{x|%Y-%m-%d}<br>Ounces: %{y}'
+    opacity=0.5,
+    hoverinfo='y+name'
 ))
+
+
+fig.add_trace(go.Scatter(
+    x = prop_.index,
+    y = (prop_['oz_exp'] * 0) - 100 , 
+    mode='lines',
+    line=dict(color='white', width=0),
+    name='',
+    yaxis='y2',
+    hoverinfo='name+x'))
 
 # Mostrar gráfica en Streamlit
 st.plotly_chart(fig, use_container_width=True)
@@ -168,3 +200,4 @@ except Exception as e:
     st.error(f"Error displaying Financial Metrics: {e}")
 
 # Ejecutar Streamlit con `streamlit run app.py`
+
