45e9e0692df1238b1ae2f6c21ad71fdf6fee6b23
Jevillalbav <jevillalbav@unal.edu.co>
2024-06-13 16:47:31 -0500

    last

--- dashboard_stream.py
+++ dashboard_stream.py
@@ -49,15 +49,19 @@ try:
     prop_ = pd.read_csv(prop_file, index_col=0, parse_dates=True)
     tt = pd.read_csv(tt_file, index_col=0)
     metrics = pd.read_csv(metrics_file, index_col=0)
-    gold = pd.read_csv(gold_file, index_col=0)
-    china = pd.read_csv(china_file, index_col=0)
-    india = pd.read_csv(india_file, index_col=0)
-    turkey = pd.read_csv(turkey_file, index_col=0)
+    gold = pd.read_csv(gold_file, index_col=0, parse_dates=True)
+    china = pd.read_csv(china_file, index_col=0, parse_dates=True)
+    india = pd.read_csv(india_file, index_col=0, parse_dates=True)
+    turkey = pd.read_csv(turkey_file, index_col=0, parse_dates=True)
     trade_history = pd.read_csv(trade_history)
 
 
 except Exception as e:
     st.error(f"Error loading data: {e}")
+
+
+for df in [gold, china, india, turkey]:
+    df = df.loc[ prop_.index[0]: ]
 
 
 # Crear pestañas
