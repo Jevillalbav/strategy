7f3fefdb772039e87c8e5657eceffd91dffc1b37
Jevillalbav <70242114+Jevillalbav@users.noreply.github.com>
2024-06-14 11:04:09 -0500

    .

--- dashboard_stream.py
+++ dashboard_stream.py
@@ -76,7 +76,7 @@ date_range = st.sidebar.slider("Select the date range", min_value=prop_.index.da
 
 if st.session_state['filter_date'] != date_range:
     st.session_state['filter_date'] = date_range
-    #st.experimental_rerun()
+    st.experimental_rerun()
 
 
 
