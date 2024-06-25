4fb65fe7f9b2dce8640dd2a1eeb0464fafc5b3b4
bloomberg <bloomberg@SRVBLOOMBERG>
2024-06-25 13:14:47 -0500

    update done the following day and hour :  2024-06-25 13:14:46

--- pages/4-Consensus Economics.py
+++ pages/4-Consensus Economics.py
@@ -3,3 +3,10 @@ import pandas as pd
 import plotly.graph_objects as go
 import datetime as dt
 
+
+st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+
+st.title("Consensus Economics - Reviews")
+
+
+st.sidebar.subheader("Report Filters")
